f31bc423c00602000d87b2ba4aeb37723733782a
Yokesh2002 <71903772+Yokesh2002@users.noreply.github.com>
2022-06-13 17:06:37 +0530

    Add files via upload

---
+++ display the nouns in while loop.py.py
@@ -0,0 +1,8 @@
+loop = 1
+while (loop < 10):
+    noun = input("Choose a noun: ")
+    p_noun = input("Choose a plural noun: ")
+    noun2 = input("Choose a noun: ")
+    place = input("Name a place: ")
+    adjective = input("Choose an adjective (Describing word): ")
+    noun3 = input("Choose a noun: ")
